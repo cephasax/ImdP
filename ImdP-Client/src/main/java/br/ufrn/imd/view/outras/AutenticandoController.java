e232d54d801005725357db07e17207b253812a2d
victorvilar <victor_vilar@hotmail.com>
2016-06-03 18:59:32 -0300

    Tela Inicial

--- ImdP-Client/src/main/java/br/ufrn/imd/view/outras/AutenticandoController.java
+++ ImdP-Client/src/main/java/br/ufrn/imd/view/outras/AutenticandoController.java
@@ -1,14 +1,8 @@
 package br.ufrn.imd.view.outras;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import br.ufrn.imd.main.ImdAuth;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.ProgressBar;
-
 import javafx.scene.image.ImageView;
 
 public class AutenticandoController {
@@ -18,20 +12,6 @@ public class AutenticandoController {
 	private ProgressBar progressbar;
 
 	private ImdAuth imdAuth;
-
-	
-	public AutenticandoController() {
-	}
-
-	@FXML
-	public void mudandoDeTela() throws IOException {
-		System.out.println("teste");
-		imdAuth.iniciarAutenticacao();
-	}
-
-	public void initialize(URL location, ResourceBundle resources) {
-
-	}
 
 	public void setMainApp(ImdAuth imdAuth) {
 		this.imdAuth = imdAuth;
